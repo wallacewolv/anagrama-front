b20515ef98bc1effa2268c75333300903b65f101
wallace <wallace_wolv@hotmail.com>
2025-01-03 12:10:39 -0300

    feat: Criando service para consumo da api fakestoreapi

---
+++ src/app/core/services/product.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Product } from '../../shared/models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiUrl = 'https://fakestoreapi.com/products';
+
+  constructor(private http: HttpClient) { }
+
+  getAllProducts(): Observable<Array<Product>> {
+    return this.http.get<Array<Product>>(this.apiUrl);
+  }
+  editProduct(product: Product): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${product.id}`, { product });
+  }
+}
